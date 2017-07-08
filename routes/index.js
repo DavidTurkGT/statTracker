06727347e8f67d540c6dae4f5948bb8a10b541a4
DavidTurkGT <dlturk747@gmail.com>
2017-07-08 09:25:44 -0400

    Connected mongoose to router

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,16 @@
 const express       = require('express');
 const router        = express.Router();
+const mongoose      = require('mongoose');
+const Schema        = mongoose.Schema;
 ////////////////////////////////////////////////////////////////////////////////
 
+//Set up Mongoose
+mongoose.connect('mongodb://127.0.0.1:27017/stats');
+mongoose.Promise = require('bluebird');
+
+//Schemas
+
+//Routes
 router.get('/', (req, res) => {
   res.send("Routes are connected!");
 });
