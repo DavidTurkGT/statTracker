a4aa23190b529c1e785aed6595bc5713930dfa82
DavidTurkGT <dlturk747@gmail.com>
2017-07-09 13:21:31 -0400

    Added PUT to /api/activities/:id

--- routes/activities.js
+++ routes/activities.js
@@ -50,21 +50,31 @@ router.post('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   //Show information about one activity I am tracking, and give me the data I have recorded for that activity.
   let activity = await Activity.findById(req.params.id)
-    .catch( (err) => res.status(404).send("Error: activity not found") );
+    .catch( (err) => res.status(400).send("Error: bad ID") );
   if(!activity) res.status(404).send("Error: activity not found");
   res.setHeader('Content-Type', 'application/json');
   res.status(200).json(activity);
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   //Update one activity I am tracking, changing attributes such as name or type. Does not allow for changing tracked data.
-  res.status(200).send("Update one activity I am tracking, changing attributes such as name or type. Does not allow for changing tracked data.");
+  let activity = await Activity.findById(req.params.id)
+    .catch( (err) => res.status(400).send("Error: bad ID") );
+  if(!activity) res.status(404).send("Error: activity not found")
+  let oldName = activity.name;
+  activity.name = req.body.name || oldName;
+  activity = await activity.save()
+    .catch( (err) => res.status(500).send("Internal server error") )
+  if(oldName === activity.name){
+    res.status(304).send("No body sent. Nothing to update!");
+  }
+  res.status(200).send("Successfully updated " + oldName + " to " + activity.name);
 });
 
 router.delete('/:id', async (req, res) => {
   //Delete one activity I am tracking. This should remove tracked data for that activity as well.
   let activity = await Activity.findById(req.params.id)
-    .catch( (err) => res.status(404).send("Error: activity not found") );
+    .catch( (err) => res.status(400).send("Error: bad ID") );
   if(!activity) res.status(404).send("Error: acitivity not found");
   activity.remove()
   .then( (activity) => res.status(200).send("Activity " + activity.name + " deleted") )
@@ -72,7 +82,7 @@ router.delete('/:id', async (req, res) => {
 });
 
 router.post('/:id/stats', (req, res) => {
-  //Add tracked data for a day. The data sent with this should include the day tracked. You can also override the data for a day already recorded.
+  //TODO: Add tracked data for a day. The data sent with this should include the day tracked. You can also override the data for a day already recorded.
   res.status(200).send("Add tracked data for a day. The data sent with this should include the day tracked. You can also override the data for a day already recorded.");
 });
 
--- routes/stats.js
+++ routes/stats.js
@@ -3,8 +3,8 @@ const router          = express.Router();
 ////////////////////////////////////////////////////////////////////////////////
 
 //Routes
-router.get('/:id', (req, res) => {
-  //Remove tracked data for a day.
+router.delete('/:id', (req, res) => {
+  //TODO: Remove tracked data for a day.
   res.status(200).send("Remove tracked data for a day.");
 });
 
